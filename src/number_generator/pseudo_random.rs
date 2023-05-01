00c72c818dfdd710086156eb2312aa7aaa4bac0b
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-05-01 15:02:31 -0500

    documentation

--- src/number_generator/pseudo_random.rs
+++ src/number_generator/pseudo_random.rs
@@ -2,12 +2,16 @@ use crate::number_generator::traits;
 use rand::Rng;
 use serde::{Deserialize, Serialize};
 
+///  A number generator that returns a pseudo-random number using the rand crate.
+///  This number is between 1 and the upper_limit.
 #[derive(Clone, Copy, Debug, Deserialize, PartialEq, Serialize)]
 pub struct PseudoRandom {
+    /// The highest number that is allowed to be generated.
     pub upper_limit: i32,
 }
 
 impl PseudoRandom {
+    /// Creates a new instance of the PseudoRandom struct.
     pub fn new(upper_limit: i32) -> Self {
         Self { upper_limit }
     }
--- src/number_generator/traits.rs
+++ src/number_generator/traits.rs
@@ -1,3 +1,5 @@
+/// A trait providing methods to generate and return an integer.
 pub trait NumberGenerator {
+    /// Generate and return an integer.
     fn generate(&self) -> i32;
 }
