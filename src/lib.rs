1696164e39e5b2c8afc32d5b91127f89177c7f8d
Lyle Cleveland <lyle.cleveland@yahoo.com>
2023-05-01 11:28:53 -0500

    add lib doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+//! Performant die that provides the ability to change the way random numbers are generated
+//! and different modes of rolling.
+
 #![warn(missing_docs)]
 
 pub mod number_generator;
